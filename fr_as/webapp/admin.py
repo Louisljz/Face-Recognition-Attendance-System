3260e6f53b00ae2326d84370aebd007ce3de5acf
Louis JZ <louis.ljz08@gmail.com>
2022-09-03 18:27:53 +0700

    imagefield

--- fr_as/webapp/admin.py
+++ fr_as/webapp/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import student_face
 
 # Register your models here.
+admin.site.register(student_face)
--- fr_as/webapp/models.py
+++ fr_as/webapp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
+import os
 # Create your models here.
+
+def rename(instance, filename):
+    main_dir = "student_faces"
+    return os.path.join(main_dir, f"{instance}.jpg")
+
+class student_face(models.Model):
+    name = models.CharField(max_length=50)
+    photo = models.ImageField(upload_to=rename)
+    
+    def __str__(self):
+        return self.name
